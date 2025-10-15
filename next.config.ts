0a9f05993cb078dd6698ce210f019e85fdeef1a3
CodeNinja-23 <santoshsunkoju@gmail.com>
2025-10-15 22:09:33 +0530

    THird 6

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* other config options here */
+
+  async redirects() {
+    return [
+      {
+        source: '/(.*)',
+        destination: 'https://www.linqrides.com/', // replace with your actual domain
+        permanent: true,
+      },
+    ];
+  },
 };
 
 export default nextConfig;
